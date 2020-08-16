af47ab85f481fef08b8996f5bbca2e442b9f5069
tagualba <tomialba16@gmail.com>
2020-08-16 16:43:51 -0300

    Home

--- src/public/js/Home.js
+++ src/public/js/Home.js
@@ -5,3 +5,43 @@ function Initializer(){
 function getCarrito(){
     document.getElementById("CarritoHTML").style.visibility = 'visible';
 }
+
+
+
+document.getElementById("nextCarruselCards1").addEventListener("click", function() {
+    document.getElementById('nextCarruselCardButton1').click();
+}, false);
+
+document.getElementById("nextCarruselCards2").addEventListener("click", function() {
+    document.getElementById('nextCarruselCardButton2').click();
+}, false);
+
+document.getElementById("nextCarruselCards3").addEventListener("click", function() {
+    document.getElementById('nextCarruselCardButton3').click();
+}, false);
+
+document.getElementById("prevCarruselCards1").addEventListener("click", function() {
+    document.getElementById('prevCarruselCardButton1').click();
+}, false);
+
+document.getElementById("prevCarruselCards2").addEventListener("click", function() {
+    document.getElementById('prevCarruselCardButton2').click();
+}, false);
+
+document.getElementById("prevCarruselCards3").addEventListener("click", function() {
+    document.getElementById('prevCarruselCardButton3').click();
+}, false);
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("nextCarruselCardButton1").addEventListener("click", function() {
+
+}, false);
